87a49c615d6396a8b740de5e92befda22f3342f4
Thomas Tortorini <thomastortorini@gmail.com>
2014-09-26 19:03:55 +0200

    # 1.4 deux nouveaux callbacks: onBeforeRemoveTab / onAfterRemoveTab

--- main.js
+++ main.js
@@ -1,16 +1,20 @@
 function lg(s) { console.log(s); }
 
 $(function() {
+	function show(cb, jq_tab, jq_content) {
+		lg('>>> ' + cb)
+		lg(jq_tab.find('span').text());
+		lg(jq_content.find('p').text().substr(0, 25));
+		lg('');
+	}
+
 	var
 		tabNumber = 0,
 		plugin_tabs = $.plugin_tabs($('#demo'), {
 			// noDragndrop: true,
 			// duration: 200,
 			onChange: function(jq_tab, jq_content) {
-				lg('>>> callback_onChange')
-				lg(jq_tab.find('span').text());
-				lg(jq_content.find('p').text().substr(0, 25));
-				lg('');
+				show('onChange', jq_tab, jq_content);
 			},
 			onNewTab: function(jq_tab, jq_content) {
 				lg('>>> callback_onNewTab')
@@ -23,6 +27,16 @@ $(function() {
 				jq_content.html(
 					'<p>NewTab: <b>'+tabNumber+'</b></p>'
 				);
+			},
+			onBeforeRemoveTab: function(jq_tab, jq_content) {
+				show('onBeforeRemoveTab', jq_tab, jq_content);
+				if (0)
+					return false;
+			},
+			onAfterRemoveTab: function(jq_tab, jq_content, container) {
+				show('onAfterRemoveTab', jq_tab, jq_content);
+				if (1 === container.getTabs().length)
+					container.newTabAppend();
 			}
 		}),
 		tabsA = plugin_tabs.container['tabs_A']
@@ -30,6 +44,4 @@ $(function() {
 
 	$('.newTabAppend') .click(function() { tabsA.newTabAppend();  });
 	$('.newTabPrepend').click(function() { tabsA.newTabPrepend(); });
-
-	window.tabsA = tabsA; // debug
 });
