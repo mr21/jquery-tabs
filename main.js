a3a0aff1266d783345234db5ec98a5705fcb3731
Thomas Tortorini <thomastortorini@gmail.com>
2015-07-30 01:44:13 +0200

    coding style

--- main.js
+++ main.js
@@ -1,49 +1,50 @@
-function lg(s) { console.log(s); }
-
 $(function() {
-	$('#moreAt')[top.location === self.location ? 'show' : 'remove']();
 
-	function show(cb, jq_tab, jq_content) {
-		lg('>>> ' + cb)
-		lg('tab     : "' + jq_tab.find('span').text() + '"');
-		lg('content : "' + jq_content.find('p').text() + '"');
-		lg('');
+	$( "#moreAt" )[ top.location === self.location ? "show" : "remove" ]();
+
+	function show( cb, jqTab, jqContent ) {
+		lg( ">>> " + cb );
+		lg( "tab     : '" + jqTab.find( "span" ).text() + "'" );
+		lg( "content : '" + jqContent.find( "p" ).text() + "'" );
+		lg( "" );
 	}
 
 	var
 		tabNumber = 0,
-		plugin_tabs = $.plugin_tabs(document.body, {
+		plugin_tabs = $.plugin_tabs( document.body, {
 			// noDragndrop: true,
 			// duration: 200,
-			onChange: function(o) {
-				show('onChange', o.jqTab, o.jqContent);
+			onChange: function( o ) {
+				show( "onChange", o.jqTab, o.jqContent );
 			},
-			onNewTab: function(o) {
-				lg('>>> onNewTab')
+			onNewTab: function( o ) {
+				lg( ">>> onNewTab" );
 				o.jqTab.html(
-					'<div>'+
-						'<span>NewTab-'+(++tabNumber)+'</span>'+
-						'<a class="jqtabs-btnCloseTab fa fa-times-circle" href="#"></a>'+
-					'</div>'
+					"<div>"+
+						"<span>NewTab-"+ (++tabNumber) +"</span>"+
+						"<a class='jqtabs-btnCloseTab fa fa-times-circle' href='#'></a>"+
+					"</div>"
 				);
 				o.jqContent.html(
-					'<p>NewTab: <b>'+tabNumber+'</b></p>'
+					"<p>NewTab: <b>"+ tabNumber +"</b></p>"
 				);
 			},
-			onBeforeRemoveTab: function(o) {
-				show('onBeforeRemoveTab', o.jqTab, o.jqContent);
-				if (0) // block the deletion or not
+			onBeforeRemoveTab: function( o ) {
+				show( "onBeforeRemoveTab", o.jqTab, o.jqContent );
+				if ( 0 ) { // block the deletion or not
 					return false;
+				}
 			},
-			onAfterRemoveTab: function(o) {
-				show('onAfterRemoveTab', o.jqTab, o.jqContent);
-				if (1 === o.jqTabsContainer.getTabs().length)
+			onAfterRemoveTab: function( o ) {
+				show( "onAfterRemoveTab", o.jqTab, o.jqContent );
+				if ( 1 === o.jqTabsContainer.getTabs().length ) {
 					o.jqTabsContainer.newTabAppend();
+				}
 			}
 		}),
-		tabsA = plugin_tabs.container['tabs_A']
+		tabsA = plugin_tabs.container[ "tabs_A" ]
 	;
 
-	$('.newTabAppend') .click(function() { tabsA.newTabAppend();  });
-	$('.newTabPrepend').click(function() { tabsA.newTabPrepend(); });
+	$( ".newTabAppend" ) .click( function() { tabsA.newTabAppend();  } );
+	$( ".newTabPrepend" ).click( function() { tabsA.newTabPrepend(); } );
 });
